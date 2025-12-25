5888b1e95a47536e04e0d5e70d96c8d1830728ba
fikile kentane <fikilekentane@gmail.com>
2025-12-25 23:34:56 +0200

    GuardedStack - logs removed

--- src/navigation/GuardedStack.js
+++ src/navigation/GuardedStack.js
@@ -6,11 +6,6 @@ import { useSelector } from "react-redux";
 import { authApi } from "../redux/authApi";
 
 export default function GuardedStack() {
-  console.log(" ");
-  console.log(" ");
-  console.log("GuardedStack ----START START");
-  console.log("GuardedStack ----START START");
-
   const router = useRouter();
 
   const authState = useSelector(
@@ -21,50 +16,21 @@ export default function GuardedStack() {
   const isAuthenticated = authState?.isAuthenticated;
   const activeWorkbaseId = authState?.profile?.access?.activeWorkbase?.id;
 
-  // const authState = useSelector(
-  //   authApi.endpoints.getAuthState.select(undefined)
-  // )?.data;
-  console.log("GuardedStack ----authState", authState);
-  // console.log(`GuardedStack ----authState`, JSON.stringify(authState, null, 2));
-
-  console.log("GuardedStack ----authState.ready", authState?.ready);
-  console.log(
-    "GuardedStack ----authState.profile?.access?.activeWorkbase?.id",
-    authState?.profile?.access?.activeWorkbase?.id
-  );
-  console.log(
-    "GuardedStack ----authState.isAuthenticated",
-    authState?.isAuthenticated
-  );
-
-  /* =========================
-     SIDE EFFECTS (NAVIGATION)
-  ========================= */
   useEffect(() => {
-    // ⛔ CRITICAL: wait until auth is resolved
-    if (!ready) {
-      console.log("GuardedStack ----ready - return", ready);
-      return;
-    }
+    if (!ready) return;
 
     if (!isAuthenticated) {
-      console.log("GuardedStack ----isAuthenticated - return", isAuthenticated);
       router.replace("/(auth)/signin");
       return;
     }
 
     if (!activeWorkbaseId) {
-      console.log(
-        "GuardedStack ----activeWorkbaseId - return",
-        activeWorkbaseId
-      );
+      // onboarding later
       return;
     }
-
-    // ✅ all good → let Slot render
   }, [ready, isAuthenticated, activeWorkbaseId, router]);
 
-  if (!authState || !authState?.ready) {
+  if (!ready) {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <ActivityIndicator size="large" />
@@ -72,7 +38,5 @@ export default function GuardedStack() {
     );
   }
 
-  // ✅ Let Slot render normally
-  console.log("GuardedStack ----Let Slot render normally ");
   return null;
 }
